7715cb65f2e460be5250bb83ee8f37a57341c386
Jesse Packwood <Jesse@Jesse.local>
2018-01-23 14:43:40 -0700

    generating random colors on page load

---
+++ public/js/scripts.js
@@ -0,0 +1,38 @@
+//need event jquery listeners
+
+//need to create random colors on page load
+
+//each color box have an id?
+
+$('lock-icon').click(changeFlag)
+
+function changeFlag() {
+  console.log('click')
+  $('lock-icon').toggleClass('selected-flag')
+}
+
+function getRandomColor() {
+  var hexCode = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += hexCode[Math.floor(Math.random() * 16)];
+  }
+  console.log(color)
+  return color;
+}
+
+function setColors() {
+  const color1 = getRandomColor();
+  const color2 = getRandomColor();
+  const color3 = getRandomColor();
+  const color4 = getRandomColor();
+  const color5 = getRandomColor();
+
+  $('#color-one').css('background', color1)
+  $('#color-two').css('background', color2)
+  $('#color-three').css('background', color3)
+  $('#color-four').css('background', color4)
+  $('#color-five').css('background', color5)
+}
+
+$(document).ready( setColors)
