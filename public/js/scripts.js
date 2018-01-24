850173ae028e0f8501835655da0d08f4193e94be
Jesse Packwood <Jesse@Jesse.local>
2018-01-23 18:00:33 -0700

    colors lock by clicking on icon

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,39 +1,42 @@
-//need event jquery listeners
-
-//need to create random colors on page load
-
-//each color box have an id?
-
 $('.lock-icon').on('click', changeFlag)
 $('#new-palette').on('click', setColors)
 
+
 function changeFlag() {
-  console.log('click')
   $(this).toggleClass('selected-flag')
+  // console.log($(this.closest()))
+  $(this).parents('.color-box').toggleClass('selected')
 }
 
 function getRandomColor() {
   var hexCode = '0123456789ABCDEF';
   var color = '#';
+  var brightness = 0;
   for (var i = 0; i < 6; i++) {
-    color += hexCode[Math.floor(Math.random() * 16)];
+    const singleChar = [Math.floor(Math.random() * 16)]
+    color += hexCode[singleChar];
+    if (i === 0 || i === 2 || i === 4) {
+      brightness += parseInt(singleChar)
+    }
   }
-  console.log(color)
-  return color;
+  console.log(brightness)
+
+  brightness < 20 ? brightness = 0 : brightness = 1;
+  console.log(brightness)
+  return [ color, brightness ];
 }
 
 function setColors() {
-  const color1 = getRandomColor();
-  const color2 = getRandomColor();
-  const color3 = getRandomColor();
-  const color4 = getRandomColor();
-  const color5 = getRandomColor();
-
-  $('#color-one').css('background', color1)
-  $('#color-two').css('background', color2)
-  $('#color-three').css('background', color3)
-  $('#color-four').css('background', color4)
-  $('#color-five').css('background', color5)
+  const boxes = $('.color-box')
+  boxes.each( function() {
+    const brightnessArray = [ '#fff', '#000'];
+    if (!$(this).hasClass('selected')) {
+    let color = getRandomColor()
+    $(this).css('background', color[0]);
+    $(this).find('.hex-code').text(color[0]).css('color', brightnessArray[color[1]])
+    }
+  })
 }
 
 $(document).ready(setColors)
+
