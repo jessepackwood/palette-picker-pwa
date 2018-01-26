884a33ab84f03d3d52f1fc740fbc495c107cd7c2
Jesse Packwood <Jesse@Jesse.local>
2018-01-26 10:24:44 -0700

    create Procfile for heroku

--- knexfile.js
+++ knexfile.js
@@ -12,4 +12,12 @@ module.exports = {
     },
     useNullasDefault: true
   }
-};
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+  },
+  useNullAsDefault: true
+  };
+}
