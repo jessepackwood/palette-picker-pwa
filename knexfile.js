8794ff2d98f2260e05a66b6e878cccb7a39c5321
Jesse Packwood <Jesse@Jesse.local>
2018-01-26 12:44:51 -0700

    add testing environment

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,19 @@ module.exports = {
     },
     useNullasDefault: true
   },
-  
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/palettepicker_test',
+    useNullasDefault: true,
+    migrations: {
+      directory: __dirname + 'db/migrations'
+    },
+    seeds: {
+      directory: './db/test'
+    }
+  },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
